b802eb2bfea33d7a92e99b6b0558701cccadb150
shankarsurya035 <shankarshakti035@gmail.com>
2021-08-16 23:23:59 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed('#boox', {
+    strings:[
+        "Hi There! I'm Surya Shankar, Strong Web development Skill with a Bachelor's degree focused in Electronics and Communication from Silicon Institute of Technology (SIT), Bhubaneswar."
+    ],
+    typeSpeed:100,
+    backSpeed:30,
+  })
